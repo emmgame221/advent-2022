131c02715f21c73eb9fdb4ecdc8ecac690f809aa
Eric McHugh <emmgame221@gmail.com>
2022-12-05 02:11:46 -0500

    day 5 cleanup

--- src/problems/day5.rs
+++ src/problems/day5.rs
@@ -12,32 +12,13 @@ pub fn print_solution() {
 }
 
 fn part_one(lines: &Vec<String>) -> String {
-    let mut stacks: Vec<VecDeque<String>> = vec![VecDeque::new();9];
-    for i in 0..8 {
-        let line = &lines[i];
-        let mut j = 1;
-        let mut stack = 0;
-        while j < line.len() {
-            let c = &line[j..j+1];
-            if c != " " {
-                stacks[stack].push_front(c.to_owned());
-            }
-            stack += 1;
-            j += 4;
-        }
-    }
-    // Note: Input was edited by hand
+    let mut stacks: Vec<VecDeque<String>> = init_stacks(lines);
     let mut i = 10;
     while i < lines.len() {
-        /*for stack in stacks.iter() {
-            print!("{:?}", stack.back());
-        }
-        println!();*/
         let mut parts = lines[i].split(' ');
         let num:usize = parts.next().unwrap().parse().unwrap();
         let from:usize = parts.next().unwrap().parse().unwrap();
         let to:usize = parts.next().unwrap().parse().unwrap();
-        //println!("num:{} from:{} to:{} from_count:{} to_count:{}", num, from, to, stacks[from - 1].len(), stacks[to - 1].len());
         for _ in 0..num {
             if let Some(top) = stacks[from - 1].pop_back(){
                 stacks[to - 1].push_back(top);
@@ -55,32 +36,13 @@ fn part_one(lines: &Vec<String>) -> String {
 }
 
 fn part_two(lines: &Vec<String>) -> String {
-    let mut stacks: Vec<VecDeque<String>> = vec![VecDeque::new();9];
-    for i in 0..8 {
-        let line = &lines[i];
-        let mut j = 1;
-        let mut stack = 0;
-        while j < line.len() {
-            let c = &line[j..j+1];
-            if c != " " {
-                stacks[stack].push_front(c.to_owned());
-            }
-            stack += 1;
-            j += 4;
-        }
-    }
-    // Note: Input was edited by hand
+    let mut stacks: Vec<VecDeque<String>> = init_stacks(lines);
     let mut i = 10;
     while i < lines.len() {
-        /*for stack in stacks.iter() {
-            print!("{:?}", stack.back());
-        }
-        println!();*/
         let mut parts = lines[i].split(' ');
         let num:usize = parts.next().unwrap().parse().unwrap();
         let from:usize = parts.next().unwrap().parse().unwrap();
         let to:usize = parts.next().unwrap().parse().unwrap();
-        //println!("num:{} from:{} to:{} from_count:{} to_count:{}", num, from, to, stacks[from - 1].len(), stacks[to - 1].len());
         let mut tempstack: VecDeque<String> = VecDeque::new();
         for _ in 0..num {
             if let Some(top) = stacks[from - 1].pop_back(){
@@ -97,4 +59,22 @@ fn part_two(lines: &Vec<String>) -> String {
         }
     }
     res
+}
+
+fn init_stacks(lines: &Vec<String>) -> Vec<VecDeque<String>> {
+    let mut stacks: Vec<VecDeque<String>> = vec![VecDeque::new();9];
+    for i in 0..8 {
+        let line = &lines[i];
+        let mut j = 1;
+        let mut stack = 0;
+        while j < line.len() {
+            let c = &line[j..j+1];
+            if c != " " {
+                stacks[stack].push_front(c.to_owned());
+            }
+            stack += 1;
+            j += 4;
+        }
+    }
+    stacks
 }
